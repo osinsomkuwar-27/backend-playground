322b81532f7a44e0fe6c7c088fb69c43421d98e8
osinsomkuwar-27 <osinsomkuwar26@gmail.com>
2026-01-05 18:34:19 +0530

    Created Edit Route which helps in editing a given message

--- mongoDBwithExpress/index.js
+++ mongoDBwithExpress/index.js
@@ -60,6 +60,13 @@ app.post("/chats", (req, res) => {
   }
 });
 
+//Edit Route
+app.get("/chats/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  let chat = await Chat.findById(id);
+  res.render("edit.ejs", { chat });
+});
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
